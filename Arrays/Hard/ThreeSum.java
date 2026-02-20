41919a0833bb5e97581cbcaef7169e72af98aab0
Srinivas Mandula <163158715+Srinivas2206@users.noreply.github.com>
2026-02-20 22:24:07 +0530

    Arrays/Hard -- 3sum and 4sum

---
+++ Arrays/Hard/ThreeSum.java
@@ -0,0 +1,38 @@
+package Arrays.Hard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                } else if (sum < 0)
+                    left++;
+                else
+                    right--;
+            }
+
+        }
+        return res;
+    }
+}
