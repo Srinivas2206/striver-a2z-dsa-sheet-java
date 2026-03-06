080b68fcf0af5d22004b3f1f1ace26e06b748f6a
Srinivas Mandula <163158715+Srinivas2206@users.noreply.github.com>
2026-03-06 10:47:27 +0530

    Arrays - Medium

--- Arrays/Medium/SortColors.java
+++ Arrays/Medium/SortColors.java
@@ -2,14 +2,12 @@
 public class SortColors {
     public void sortColors(int[] nums) {
         int n = nums.length;
-        int count_0 = 0, count_1 = 0, count_2 = 0;
+        int count_0 = 0, count_1 = 0;
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
                 count_0++;
             } else if (nums[i] == 1) {
                 count_1++;
-            } else {
-                count_2++;
             }
         }
 
